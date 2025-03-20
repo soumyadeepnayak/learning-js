973127d976e45f3ced048f9a8d7408bed691ebf6
Soumyadeep <Soumyadeep1211@outlook.com>
2025-03-20 21:21:34 +0530

    type conversion

---
+++ 01_variable/03_datatypeconversion.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN ; NaN => Not a Number ; typeof NaN is => number
+// true => 1 ; false => 0
+// null => 0 
+
+
+let isLoggedIn = " "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0=> false
+// "" => false ; " " => true
+// null => false ; 0 => false
+// "soumyadeep" => true ; "0" => true
+
+let someNumber = 452
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
