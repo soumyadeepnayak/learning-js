272b11325919f5da36e7328d99da18a944559912
Soumyadeep <Soumyadeep1211@outlook.com>
2025-03-20 19:15:57 +0530

    datatype

---
+++ 01_variable/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all JS code as newer version 
+
+// alert(3+3) // we all are using nodejs not browser 
+
+console.log(3+3)
+
+console.log("soumyadeep")
+
+//Different types of data types in JS
+// 1. String => "soumyadeep"
+// 2. Number => 1234 n range is 2^53
+// 3. Boolean => true or false
+// 4. Undefined => variable is declared but not defined
+// 5. Null => variable is declared and defined but value is null
+// 6. BigInt
+// 7. Symbol => unique value
+// 8. Object => {key: value}
+
+console.log(typeof "soumyadeep")
+console.log(typeof 1234)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
