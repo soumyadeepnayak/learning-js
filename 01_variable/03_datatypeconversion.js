9910c02849454a1db4cc49d9d565eb01895bf674
Soumyadeep <Soumyadeep1211@outlook.com>
2025-03-23 17:30:42 +0530

    type conversion done

--- 01_variable/03_datatypeconversion.js
+++ 01_variable/03_datatypeconversion.js
@@ -25,5 +25,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 452
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(1+"1")
+// console.log("1"+1)
+// console.log("1"+"1")
+// console.log("1"+2+1)  // output is 121 because first one is string then it will treat all as string  
+// console.log(1+2+"1") // output is 31 because first two are number then it will add first two then it will treat as string
+
+console.log(true) // output is true
+console.log(+true) // output is 1
+console.log(+"") // output is 0
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter) // output is 101
+++gameCounter
+console.log(gameCounter) // output is 102
+//read about postfix and prefix operator from mdn
